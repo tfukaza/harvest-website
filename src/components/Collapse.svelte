8509e631700ec726dcab598a5c2ed7d1ff758e87
tfukaza <tfukaza@git.com>
2024-06-24 21:23:15 -0700

    Astro

---
+++ src/components/Collapse.svelte
@@ -0,0 +1,58 @@
+
+<script lang="ts">
+	import { BASE } from '@constants';
+	export let title: string = '';
+	export let items: { name: string; }[] = [];
+
+	let collapsed = false;
+
+	function onToggleCollapse(_: MouseEvent) {
+		collapsed = !collapsed;
+	}
+</script>
+
+<div class={collapsed ? 'collapse' : ''}>
+	<button class="collapseButton" on:click={onToggleCollapse}><span class="collapseIcon">▸</span>{title}</button>
+	<ul>
+		{#each items as item (`${title.toLowerCase()}-${item.name}`)}
+			<li id={`link-${item.name}`}>
+				<a href={BASE + `/docs/${title.replace(' ', '-').toLowerCase()}/${item.name}`}>{item.name}</a>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.collapseButton {
+		width: 100%;
+		padding: 4px 0px 4px 8px;
+		
+		background-color: transparent;
+		border: none;
+		border-radius: 4px;
+
+		text-align: left;
+		font-family: 'Inter', sans-serif;
+
+		cursor: pointer;
+
+		&:hover {
+			background-color: rgb(243, 243, 243);
+		}
+	}
+
+	.collapseIcon {
+		display: inline-block;
+		transform: rotate(90deg);
+		transition: transform 0.1s;
+	}
+
+	.collapse {
+		& ul {
+			display: none;
+		}
+		& .collapseIcon {
+			transform: rotate(0deg);
+		}
+	}
+</style>
---
+++ src/components/landing/MixMatch.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+</script>
+
+<section id="mix-n-match">
+    <!-- <h2>Mix n' Match</h2>
+    <p>The library provides a common API for brokers and databases, 
+        so you can seamlessly connect trading strategies with your favorite tech stack.</p>
+
+    <div class="icon">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg" alt="Tech Stack" />
+    </div>
+
+    <div id="harvest-icon">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg" alt="Tech Stack" />
+    </div> -->
+</section>
+
+<style>
+    #mix-n-match {
+        background-color: var(--bg-light);
+        text-align: center;
+    }
+
+    .icon {
+        width: 64px;
+        height: 64px;
+        border-radius: 16px;
+        box-shadow: 0px 7px 19px rgb(23 29 45 / 14%);
+        padding: 10px;
+    }
+
+    #harvest-icon {
+        width: 128px;
+        height: 128px;
+        border-radius: 16px;
+        box-shadow: 0px 20px 30px -8px rgb(23 29 45 / 14%);
+        padding: 10px;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+</style>
